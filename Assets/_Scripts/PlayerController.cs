4cbbad15909069898451048c1557213186321899
cesar barrueta <cesarantonio180@gmail.com>
2024-02-08 19:16:38 -0600

    Add player movement script

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    private bool isMoving;
+    public float speed;
+    private Vector2 input;
+    private Animator _animator;
+    public LayerMask solidObjectsLayer;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if(input.x !=0)
+            {
+                input.y = 0;
+            }
+
+            if (input != Vector2.zero)
+            {
+                _animator.SetFloat("moveX", input.x);
+                _animator.SetFloat("moveY", input.y);
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+
+                if(IsAvailable(targetPosition))
+                {
+                    StartCoroutine(MoveTowards(targetPosition));
+                }
+                
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        _animator.SetBool("is Moving", isMoving);
+    }
+
+    IEnumerator MoveTowards (Vector3 destination) 
+    {
+        isMoving = true;
+        while(Vector3.Distance(transform.position, destination) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null; //Espera hasta que termine el frame
+        }
+
+        transform.position = destination;
+        isMoving = false;
+    }
+
+    /// <summary>
+    /// Comprueba que la zona a acceder esté disponible (no sea sólido)
+    /// </summary>
+    /// <param name="target">Zona a acceder</param>
+    /// <returns>Devuelve true si el target está disponible</returns>
+    private bool IsAvailable(Vector3 target)
+    {
+        if(Physics2D.OverlapCircle(target, 0.2f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+}
