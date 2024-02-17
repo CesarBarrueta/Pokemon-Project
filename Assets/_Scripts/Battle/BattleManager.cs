36e1e2d9292a50ead318b0f89f0f05852d1a62da
cesar barrueta <cesarantonio180@gmail.com>
2024-02-16 20:20:46 -0600

    Update BattleManager (Include BattleDialogBox logic)

--- Assets/_Scripts/Battle/BattleManager.cs
+++ Assets/_Scripts/Battle/BattleManager.cs
@@ -2,6 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState 
+{
+    StartBattle,
+    PlayerSelectAction,
+    PlayerMove,
+    EnemyMove,
+    Busy
+};
+
 public class BattleManager : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
@@ -12,19 +21,48 @@ public class BattleManager : MonoBehaviour
 
     [SerializeField] BattleDialogBox battleDialogBox;
 
+    public BattleState state;
+
     public void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    public IEnumerator SetupBattle()
     {
+        state = BattleState.StartBattle;
+
         playerUnit.SetupPokemon();
         playerHUD.SetPkmnData(playerUnit.Pokemon);
 
         enemyUnit.SetupPokemon();
         enemyHUD.SetPkmnData(enemyUnit.Pokemon);
 
-        StartCoroutine(battleDialogBox.SetDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared!"));
+        yield return battleDialogBox.SetDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared!");
+        yield return new WaitForSeconds(1.0f);
+
+        //TODO: Comparar speed de EnemyUnit y PlayerUnit para decidir quien ataca primero}
+        if(enemyUnit.Pokemon.Speed > playerUnit.Pokemon.Speed)
+        {
+            StartCoroutine(battleDialogBox.SetDialog($"Enemy {enemyUnit.Pokemon.Base.Name} attacks first"));
+            EnemyAction();
+        }else
+        {
+            StartCoroutine(battleDialogBox.SetDialog($"{playerUnit.Pokemon.Base.Name} attacks first"));
+            PlayerAction();
+        }
+
+    }
+
+    public void PlayerAction()
+    {
+        state = BattleState.PlayerSelectAction;
+        StartCoroutine(battleDialogBox.SetDialog("Select an action"));
+        battleDialogBox.ToggleActionsText(true);
+    }
+
+    public void EnemyAction()
+    {
+
     }
 }
